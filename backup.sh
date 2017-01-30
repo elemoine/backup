4c4c8f2cdb7d15cae6ad1868ef7a71757658e874
Éric Lemoine <eric.lemoine@oslandia.com>
2017-01-30 08:44:33 +0100

    Improve backup.sh script

--- backup.sh
+++ backup.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-duplicity --exclude-filelist excludes.txt ${HOME} file:///media/usb/backup
+[[ -n $DEV ]] || DEV="/dev/sdb"
+
+error() {
+    local errmsg="$1"
+    echo "Error: ${errmsg}"
+    exit 1
+}
+
+main() {
+    local mountpoint=$(findmnt -n -f -o target ${DEV})
+    [[ -n $mountpoint ]] || error "${DEV} is not mounted"
+
+    local backupdir=${mountpoint}/backup
+    [[ -d $backupdir ]] || error "${backupdir} does not exist"
+
+    duplicity --exclude-filelist excludes.txt ${HOME} file://${backupdir}
+}
+
+main
