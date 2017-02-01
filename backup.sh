737322b69ee114d01b0f44454e16bc43a3cba70a
Éric Lemoine <eric.lemoine@oslandia.com>
2017-02-01 23:06:57 +0100

    Correctly use gpg-agent

--- backup.sh
+++ backup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+readonly PROGNAME=$(basename $0)
+readonly ARGS="$@"
+readonly USERID=$(id --user --name)
+
 usage() {
-    echo "Usage: $0 [-l <dev>] [-r]"
+    echo "Usage: ${PROGNAME} -d <backup_directory> -k <key_id> [-x]"
     exit 1
 }
 
@@ -16,48 +20,63 @@ log_info() {
     echo "${msg}"
 }
 
-while getopts ":l:r" opt; do
-    case "${opt}" in
-        l)
-            lbackup="yes"
-            lbackupdev=${OPTARG}
-            ;;
-        r)
-            rbackup="yes"
-            ;;
-        \?)
-            usage
-            ;;
-    esac
-done
+log_debug() {
+    local msg="$1"
+    [[ -n ${BACKUP_DEBUG} ]] && echo "${msg}"
+}
+
+_do_backup() {
+    local target=$1
+    local backup_keyid=$2
+    log_debug "Backup target: ${target}"
+    log_debug "Backup key: ${backup_keyid}"
+    local opts="--encrypt-sign-key ${backup_keyid} --use-agent --exclude-filelist excludes.txt"
+    [[ -n ${BACKUP_DEBUG} ]] && opts="--verbosity info ${opts}"
+    duplicity ${opts} ${HOME} ${target}
+}
+
+do_backup_file() {
+    local backup_dir=$1
+    local backup_keyid=$2
+    [[ -d ${backup_dir} ]] || error "${backup_dir} does not exist"
+    _do_backup "file://${backup_dir}" "${backup_keyid}"
+}
+
+do_backup_scp() {
+    local backup_host=$1
+    local backup_keyid=$2
+    local backup_dir="/share/MD0_DATA/homes/admin/backups/workstations/${USERID}"
+    _do_backup "scp://${backup_host}/${backup_dir}" "${backup_keyid}"
+}
 
 main() {
 
-    if [[ ${lbackup} == "yes" ]]; then
-        #
-        # local backup
-        #
-        local mountpoint=$(findmnt -n -f -o target ${lbackupdev})
-        [[ -n ${mountpoint} ]] || error "${lbackupdev} is not mounted"
-        log_info "Mount point: ${mountpoint}"
+    while getopts ":d:h:k:x" opt
+    do
+        case "${opt}" in
+            d)
+                readonly BACKUP_DIR=${OPTARG}
+                ;;
+            h)
+                readonly BACKUP_HOST=${OPTARG}
+                ;;
+            k)
+                readonly BACKUP_KEYID=${OPTARG}
+                ;;
+            x)
+                declare -g -r BACKUP_DEBUG=1
+                set -x
+                ;;
+            \?)
+                usage
+                ;;
+        esac
+    done
 
-        local backupdir=${mountpoint}/backup
-        [[ -d ${backupdir} ]] || error "${backupdir} does not exist"
-        log_info "Local backup directory: ${backupdir}"
+    [[ -n ${BACKUP_DIR} ]] || usage
+    [[ -n ${BACKUP_KEYID} ]] || usage
 
-        duplicity --use-agent --verbosity info --exclude-filelist excludes.txt ${HOME} file://${backupdir}
-    fi
-
-    if [[ ${rbackup} == "yes" ]]; then
-        #
-        # remote backup
-        #
-        local username=$(id --user --name)
-        local backupdir="/share/MD0_DATA/homes/admin/backups/workstations/${username}"
-        log_info "Remote backup directory: ${backupdir}"
-
-        duplicity --use-agent --verbosity info --exclude-filelist excludes.txt ${HOME} scp://backup/${backupdir}
-    fi
+    do_backup_file ${BACKUP_DIR} ${BACKUP_KEYID}
 }
 
-main
+main ${ARGS}
