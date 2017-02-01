86b9eab4d567aa10926ba7c12ce50d759ec222d4
Éric Lemoine <eric.lemoine@oslandia.com>
2017-02-01 10:41:21 +0100

    Improve backup.sh script

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-[[ -n $DEV ]] || DEV="/dev/sdb"
+usage() {
+    echo "Usage: $0 [-l <dev>] [-r]"
+    exit 1
+}
 
 error() {
     local errmsg="$1"
@@ -8,12 +11,35 @@ error() {
     exit 1
 }
 
+log_info() {
+    local msg="$1"
+    echo "${msg}"
+}
+
+while getopts ":l:r" opt; do
+    case "${opt}" in
+        l)
+            dev=${OPTARG}
+            ;;
+        r)
+            usage
+            ;;
+        \?)
+            usage
+            ;;
+    esac
+done
+
 main() {
-    local mountpoint=$(findmnt -n -f -o target ${DEV})
-    [[ -n $mountpoint ]] || error "${DEV} is not mounted"
+    [[ -n ${dev} ]] || error "no device defined"
+
+    local mountpoint=$(findmnt -n -f -o target ${dev})
+    [[ -n ${mountpoint} ]] || error "${DEV} is not mounted"
+    log_info "Mount point: $mountpoint"
 
     local backupdir=${mountpoint}/backup
-    [[ -d $backupdir ]] || error "${backupdir} does not exist"
+    [[ -d ${backupdir} ]] || error "${backupdir} does not exist"
+    log_info "Backup directory: ${backupdir}"
 
     duplicity --exclude-filelist excludes.txt ${HOME} file://${backupdir}
 }
