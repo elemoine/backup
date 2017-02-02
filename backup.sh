528a99ab6bce0ade283c880a0e1fa25de9540bf2
Éric Lemoine <eric.lemoine@oslandia.com>
2017-02-02 12:03:46 +0100

    Improve both backup.sh and restore.sh

--- backup.sh
+++ backup.sh
@@ -15,35 +15,12 @@ Usage: ${PROGNAME} options
 OPTIONS:
     -d <dir>      the backup target directory
     -k <key>      the GPG key
+    -x            increase verbosity
 
 Example:
     ${PROGNAME} -d /media/usb/backup -k E588ECCD
 EOF
     exit $exitcode
-}
-
-_do_backup() {
-    local target=$1
-    local backup_keyid=$2
-    log_debug "Backup target: ${target}"
-    log_debug "Backup key: ${backup_keyid}"
-    local opts="--encrypt-sign-key ${backup_keyid} --use-agent --exclude-filelist excludes.txt"
-    [[ -n ${BACKUP_DEBUG} ]] && opts="--verbosity info ${opts}"
-    duplicity ${opts} ${HOME} ${target}
-}
-
-do_backup_file() {
-    local backup_dir=$1
-    local backup_keyid=$2
-    [[ -d ${backup_dir} ]] || error "${backup_dir} does not exist"
-    _do_backup "file://${backup_dir}" "${backup_keyid}"
-}
-
-do_backup_scp() {
-    local backup_host=$1
-    local backup_keyid=$2
-    local backup_dir="/share/MD0_DATA/homes/admin/backups/workstations/${USERID}"
-    _do_backup "scp://${backup_host}/${backup_dir}" "${backup_keyid}"
 }
 
 main() {
@@ -61,8 +38,7 @@ main() {
                 readonly BACKUP_KEYID=${OPTARG}
                 ;;
             x)
-                declare -g -r BACKUP_DEBUG=1
-                set -x
+                declare -g -r BACKUP_RESTORE_DEBUG=1
                 ;;
             \?)
                 usage 1
@@ -73,7 +49,7 @@ main() {
     [[ -n ${BACKUP_DIR} ]] || usage 1
     [[ -n ${BACKUP_KEYID} ]] || usage 1
 
-    do_backup_file ${BACKUP_DIR} ${BACKUP_KEYID}
+    do_backup_home_with_file ${BACKUP_DIR} ${BACKUP_KEYID}
 }
 
 main ${ARGS}
--- restore.sh
+++ restore.sh
@@ -1,3 +1,64 @@
 #!/bin/bash
 
-duplicity file:///media/usb/backup ${HOME}
+. "functions.sh"
+
+readonly PROGNAME=$(basename $0)
+readonly PROGDIR=$(readlink -m $(dirname $0))
+readonly ARGS="$@"
+readonly USERID=$(id --user --name)
+
+usage() {
+    local exitcode=$1
+    cat <<- EOF
+Usage: ${PROGNAME} options
+
+OPTIONS:
+    -s <dir>      the source directory
+    -k <key>      the GPG key
+    -n            restore in a temp dir instead of ${HOME}
+    -x            increase verbosity
+
+Example:
+    ${PROGNAME} -s /media/usb/backup -k E588ECCD -n
+EOF
+    exit $exitcode
+}
+
+main() {
+
+    while getopts ":s:h:k:xn" opt
+    do
+        case "${opt}" in
+            s)
+                readonly RESTORE_DIR=${OPTARG}
+                ;;
+            h)
+                usage 0
+                ;;
+            k)
+                readonly RESTORE_KEYID=${OPTARG}
+                ;;
+            x)
+                declare -g -r BACKUP_RESTORE_DEBUG=1
+                ;;
+            n)
+                readonly RESTORE_TEST=1
+                ;;
+            \?)
+                usage 1
+                ;;
+        esac
+    done
+
+    [[ -n ${RESTORE_DIR} ]] || usage 1
+    [[ -n ${RESTORE_KEYID} ]] || usage 1
+
+    if [[ -n ${RESTORE_TEST} ]];
+    then
+        do_restore_test_with_file ${RESTORE_DIR} ${RESTORE_KEYID}
+    else
+        do_restore_home_with_file ${RESTORE_DIR} ${RESTORE_KEYID}
+    fi
+}
+
+main ${ARGS}
