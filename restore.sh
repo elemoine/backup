9d43c48020d2e9fdb8ea93b99e6356bb5c07962f
Éric Lemoine <eric.lemoine@oslandia.com>
2018-06-28 08:55:44 +0200

    Add dry-run option to restore.sh

--- restore.sh
+++ restore.sh
@@ -19,6 +19,7 @@ OPTIONS:
     -t            restore in a temp dir instead of ${HOME}
     -h            help
     -v            increase verbosity
+    -n            dry-run
 
 Example:
     ./${PROGNAME} -d /media/usb/backup -s 5BBF59DF126FADEF -e 57F334375840CA38 -t -v
@@ -31,37 +32,42 @@ _do_restore() {
     local restore_target=$2
     local restore_sign_key=$3
     local restore_encrypt_key=$4
+    local restore_dryrun=$5
     log_debug "Restore source: ${restore_source}"
     log_debug "Restore target: ${restore_target}"
     log_debug "Restore sign key: ${restore_sign_key}"
     log_debug "Restore encrypt key: ${restore_encrypt_key}"
     local opts="--sign-key ${restore_sign_key} --encrypt-key ${restore_encrypt_key} --use-agent"
     [[ -n ${BACKUP_RESTORE_DEBUG} ]] && opts="--verbosity info ${opts}"
+    [[ -n ${restore_dryrun} ]] && opts="--dry-run ${opts}"
     duplicity ${opts} ${restore_source} ${restore_target}
+    log_info "Command run: duplicity ${opts} ${restore_source} ${restore_target}"
 }
 
 do_restore_home_with_file() {
     local restore_dir=$1
     local restore_sign_key=$2
     local restore_encrypt_key=$3
+    local restore_dryrun=$4
     [[ -d ${restore_dir} ]] || error "${restore_dir} does not exist"
-    _do_restore "file://${restore_dir}" "${HOME}" "${restore_sign_key}" "${restore_encrypt_key}"
+    _do_restore "file://${restore_dir}" "${HOME}" "${restore_sign_key}" "${restore_encrypt_key}" "${restore_dryrun}"
 }
 
 do_restore_test_with_file() {
     local restore_dir=$1
     local restore_sign_key=$2
     local restore_encrypt_key=$3
+    local restore_dryrun=$4
     [[ -d ${restore_dir} ]] || error "${restore_dir} does not exist"
     rm -rf /tmp/restore-test.* # remove any previous restore test dir
     local temp_dir=$(mktemp -d -p /tmp restore-test.XXXXXXXXXX)
     log_info "Restore test target directory: ${temp_dir}"
-    _do_restore "file://${restore_dir}" "${temp_dir}" "${restore_sign_key}" "${restore_encrypt_key}"
+    _do_restore "file://${restore_dir}" "${temp_dir}" "${restore_sign_key}" "${restore_encrypt_key}" "${restore_dryrun}"
 }
 
 main() {
 
-    while getopts ":d:s:e:thv" opt
+    while getopts ":d:s:e:thvn" opt
     do
         case "${opt}" in
             d)
@@ -82,6 +88,9 @@ main() {
             v)
                 declare -g -r BACKUP_RESTORE_DEBUG=1
                 ;;
+            n)
+                readonly RESTORE_DRYRUN=1
+                ;;
             \?)
                 usage 1
                 ;;
@@ -94,9 +103,9 @@ main() {
 
     if [[ -n ${RESTORE_TEST} ]];
     then
-        do_restore_test_with_file ${RESTORE_DIR} ${RESTORE_SIGN_KEY} ${RESTORE_ENCRYPT_KEY}
+        do_restore_test_with_file ${RESTORE_DIR} ${RESTORE_SIGN_KEY} ${RESTORE_ENCRYPT_KEY} ${RESTORE_DRYRUN}
     else
-        do_restore_home_with_file ${RESTORE_DIR} ${RESTORE_SIGN_KEY} ${RESTORE_ENCRYPT_KEY}
+        do_restore_home_with_file ${RESTORE_DIR} ${RESTORE_SIGN_KEY} ${RESTORE_ENCRYPT_KEY} ${RESTORE_DRYRUN}
     fi
 }
 
